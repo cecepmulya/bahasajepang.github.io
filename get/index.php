c9a7f66650dedd8584366a3c02e7c24f8f1ebec4
Cecep Mulya <mulyadicecep@gmail.com>
2015-11-12 01:38:45 +0700

    Update index.php

--- get/index.php
+++ get/index.php
@@ -1,13 +1,10 @@
 <?php
 $page = $_GET['page'];
-include 'header.php';
+include '../header.php';
 function bacaHTML($url){
-     // inisialisasi CURL
      $data = curl_init();
-     // setting CURL
      curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
      curl_setopt($data, CURLOPT_URL, $url);
-     // menjalankan CURL untuk membaca isi file
      $hasil = curl_exec($data);
      curl_close($data);
      return $hasil;
@@ -29,15 +26,15 @@ $pecahLagi[0] = str_replace('Free Android app', '', $pecahLagi[0]);
 $pecahLagi[0] = str_replace('<a href="http://www.goethe-verlag.com/book2/_ios_app/IDJA.HTM" target="_blank">', '', $pecahLagi[0]);
 $pecahLagi[0] = str_replace('Free iPhone app</a>', '', $pecahLagi[0]);
 $pecahLagi[0] = str_replace('<img src="../../', '<img src="http://www.goethe-verlag.com/book2/', $pecahLagi[0]);
-$pecahLagi[0] = str_replace('<a href="', '<a target="_blank" href="http://www.ccpro.ml/get/?page=', $pecahLagi[0]);
-$pecahLagi[0] = str_replace('/get/?page=IDJA002.HTM', '/', $pecahLagi[0]);
+$pecahLagi[0] = str_replace('<a href="', '<a target="_blank" href="http://www.ccpro.ml/jpid/get/?page=', $pecahLagi[0]);
+$pecahLagi[0] = str_replace('/jpid/get/?page=IDJA002.HTM', '/', $pecahLagi[0]);
 $pecahLagi[0] = str_replace('http://www.goethe-verlag.com/book2/_an_app/IDJA.HTM" target="_blank">', '', $pecahLagi[0]);
-$pecahLagi[0] = str_replace('</a><a target="_blank" href="http://www.ccpro.ml/get/?page=', '', $pecahLagi[0]);
-$pecahLagi[0] = str_replace('<a target="_blank" href="http://www.ccpro.ml/get/?page=http://', '<a target="_blank" href="http://', $pecahLagi[0]);
+$pecahLagi[0] = str_replace('</a><a target="_blank" href="http://www.ccpro.ml/jpid/get/?page=', '', $pecahLagi[0]);
+$pecahLagi[0] = str_replace('<a target="_blank" href="http://www.ccpro.ml/jpid/get/?page=http://', '<a target="_blank" href="http://', $pecahLagi[0]);
 $pecahLagi[0] = str_replace('" target="_blank">', '">', $pecahLagi[0]);
 $pecahLagi[0] = str_replace('target="_blank" class="Stil40"', 'class="Stil40"', $pecahLagi[0]);
 $pecahLagi[0] = str_replace('www.goethe-verlag.com/tests/JY/JY.HTM', 'id.wikipedia.org/wiki/Bahasa_Jepang', $pecahLagi[0]);
 $pecahLagi[0] = str_replace('www.goethe-verlag.com/poster/indexeng.htm', 'id.wikipedia.org/wiki/Bahasa_Jepang', $pecahLagi[0]);
 echo $pecahLagi[0];
-include 'footer.php';
+include '../footer.php';
 ?>
