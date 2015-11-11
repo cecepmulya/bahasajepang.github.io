7c93337c9473c55fbc34ebb630c28916441c5464
Cecep Mulya <mulyadicecep@gmail.com>
2015-11-12 01:35:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 $page = $_GET['page'];
 include '/header.php';
 function bacaHTML($url){
-     // inisialisasi CURL
      $data = curl_init();
-     // setting CURL
      curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
      curl_setopt($data, CURLOPT_URL, $url);
-     // menjalankan CURL untuk membaca isi file
      $hasil = curl_exec($data);
      curl_close($data);
      return $hasil;
